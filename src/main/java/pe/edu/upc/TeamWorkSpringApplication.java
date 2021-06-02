544566b63a3f376bed8e7305ad3289a6be875fd1
Fabrizio <u201621272@upc.edu.pe>
2021-06-02 17:04:28 -0500

    primer commit

---
+++ src/main/java/pe/edu/upc/TeamWorkSpringApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamWorkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamWorkSpringApplication.class, args);
+	}
+
+}
